0eea2606937a375ee87b31d5059daf1d1020d165
Darina Baltaeva <darina-baltaeva@yandex.ru>
2023-09-13 22:12:33 +0300

    Исправлена задача

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,33 +1,32 @@
-const menuElements = document.querySelectorAll('menu__link');
-const parentMenu = li.closest('menu menu_sub menu_active');
+const menuLink = document.getElementsByClassName("menu__link");
+const menuActive = document.getElementsByClassName("menu menu_sub menu_active");
 
 function clearActive() {
-    arrayActive = Array.from(parentMenu);
-    if (arrayActive.length === 1) {
+    arrayActive = Array.from(menuActive);
+    if (arrayActive.length >= 1) {
         for (let i = 0; i < arrayActive.length; i++) {
             arrayActive[i].className = "menu menu_sub";
         }
     }
 }
 
-for (let element of menuElements) {
-    element.onclick = function() {
-        if (!parentMenu) { 
-            return
+for (let i = 0; i < Array.from(menuLink).length; i++) {
+    menuLink[i].onclick = function() {
+        const menuSub = this.closest("li").querySelector("ul");
+        if (!menuSub) {
+            return;
+        }
+
+        if (menuSub.className === "menu menu_sub") {
+            clearActive();
+            menuSub.className = "menu menu_sub menu_active";
+            return false;
+        } else {
+            menuSub.className = "menu menu_sub";
+            return false;
         }
     }
-}   
-
-if (parentMenu.className === "menu menu_sub") {
-    clearActive();
-    parentMenu.className = "menu menu_sub menu_active";
-    return false;
-} else {
-    parentMenu.className = "menu menu_sub";
-    return false;
 }
-
-
 
 
 
